ab1295cad5c0864d371d2054df164ddd323ad9e1
Aedan Yturralde <aedanyturralde@gmail.com>
2021-11-12 17:23:10 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,17 @@
+anmial = "wolverine"
+
+is_cuddly = false
+
+anmial = "vulture"
+
+num_8 = 8
+
+num_3 = 3
+
+quotient = num_3/num_8
+
+favorite_films = ["Kill Bill", "Deathproof", "Corpse Bride"]
+
+favorite_films.shift
+
+anmial.length
