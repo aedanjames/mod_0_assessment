f0d86d32b0d6f261e46be02013bc7c0f8c45033b
Aedan Yturralde <aedanyturralde@gmail.com>
2021-11-12 17:50:31 -0700

    Add house model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+my_house = {
+  street_address: "3425 W Mansfield ave",
+  city: "Denver",
+  lot_size_feet: 6970,
+  square_feet: 1200,
+  has_fireplace: false,
+  has_basement: true,
+  resident_animals: ["Tesla", "Mantis", "Mushu"],
+  appliances: ["Fridge", "washer", "dryer", "boiler", "mini-split"]
+}
