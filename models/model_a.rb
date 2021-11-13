41ab9db49846db62d1ba7206a4a1c88aea12075a
Aedan Yturralde <aedanyturralde@gmail.com>
2021-11-12 17:38:57 -0700

    Add house model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+alienware_x17_laptop = {
+processor: "11th gen Intel Core i7",
+video_card: "NVIDIA GeForce RTX 3060 6GB",
+memory_gb: 16,
+hard_drive_capacity: 256,
+is_expensive: true,
+has_ssd: true,
+keyboard_colors: ["red", "orange", "green", "purple"],
+ports: ["headset jack", "type C USB ports", "Ethernet", "HDMI 2.1"]
+}
